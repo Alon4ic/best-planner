c296b55b4d4ba3995e0516e34bad9f95a2015d09
Alon4ic <o.redjko@gmail.com>
2024-03-07 11:12:33 +0200

    FIles of back-end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': 1,
-    'prettier/prettier': 0,
-  },
-};
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+		'eslint:recomended'
+	],
+	root: true,
+	env: {
+		node: true,
+		jest: true
+	},
+	ignorePatterns: ['.eslintrc.js'],
+	rules: {
+		'@typescript-eslint/interface-name-prefix': 'off',
+		'@typescript-eslint/explicit-function-return-type': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'no-console': 1,
+		'prettier/prettier': 0,
+		'@typescript-eslint/no-unused-vars': 'off'
+	}
+}
---
+++ src/pomodoro/pomodoro.dto.ts
@@ -0,0 +1,19 @@
+import { Transform } from 'class-transformer'
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsString } from 'class-validator'
+import { Priority } from 'prisma/generated/client'
+
+
+export class PomodoroSessionDto {
+	@IsOptional()
+	@IsBoolean()
+	isCompleted: boolean
+}
+
+export class PomodoroRoundDto {
+	@IsNumber()
+	totalSeconds: number
+
+	@IsOptional()
+	@IsBoolean()
+	isCompleted:boolean
+}
